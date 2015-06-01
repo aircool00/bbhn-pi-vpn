fbbbcae5a66da7f444945e7960aa063706b4394d
bwattendorf <bwattendorf@gmail.com>
2015-06-01 17:51:40 -0400

    started to create vtund start template

--- src/var/www/hsmm-pi/Controller/VpnSettingsController.php
+++ src/var/www/hsmm-pi/Controller/VpnSettingsController.php
@@ -116,7 +116,8 @@ Public function email(){
 }
 
 Public function createvpnconf(){
- $this->create_first_vtund();
+ $this->render_vtundstart();
+ $this->render_vtun_config();
  
  return $this->redirect(array('action' => 'index'));
 
@@ -134,98 +135,43 @@ Public function createvpnconf(){
 	$file->append("	firewall  /sbin/ipchains;\r\n");
 	$file->append("	syslog daemon;\r\n");
 	$file->append("	timeout 60;\r\n");
-	$file->append("}\r\n\");
+	$file->append("}\r\n");
 	$file->append("\r\n");
-	$file->append("default {\r\n");
+	$file->append("default {"\r\n");
 	$file->append("	compress yes;\r\n");
 	$file->append("	speed 0;\r\n");
 	$file->append("	stat yes;\r\n");
 	$file->append("	}\r\n");
+	$file->append(['NetworkSetting']['server_password'])
 	}
 
+	
+	
+	
+	
 //this is a for vtund.conf to add the callsign, ip addresses, and passwords 
-  private function render_vpn_client_config($network_setting)  {
-	$vpnclient_conf = file_get_contents(WWW_ROOT . "/files/VPN/vtund.conf.template");
-	$vpnclient_conf_output = str_replace(array('{server_port}','{callsign}','{server_password}','{client_ip}','{server_ip}',
-	                                            '{client_one_callsign}','{client_one_password}','{client_one_server_ip}','{client_one_ip}',
-												'{client_two_callsign}','{client_two_password}','{client_two_server_ip}','{client_two_ip}',
-												'{client_three_callsign}','{client_three_password}','{client_three_server_ip}','{client_three_ip}',
-												'{client_four_callsign}','{client_four_password}','{client_four_server_ip}','{client_four_ip}',
-												'{client_five_callsign}','{client_five_password}','{client_five_server_ip}','{client_five_ip}',
-												'{client_six_callsign}','{client_six_password}','{client_six_server_ip}','{client_six_ip}',
-												'{client_seven_callsign}','{client_seven_password}','{client_seven_server_ip}','{client_seven_ip}',
-												'{client_eight_callsign}','{client_eight_password}','{client_eight_server_ip}','{client_eight_ip}',
-												'{client_nine_callsign}','{client_nine_password}','{client_nine_server_ip}','{client_nine_ip}',
-												'{client_ten_callsign}','{client_ten_password}','{client_ten_server_ip}','{client_ten_ip}'),
+  private function render_vtundstart($network_setting)  {
+	$vpnclient_conf = file_get_contents(WWW_ROOT . "/files/VTUND/vtundstart.conf.template");
+	$vpnclient_conf_output = str_replace(array('{server_port}','{callsign}','{server_password}','{client_ip}','{server_ip}'),
 												
-						array( $network_setting['NetworkSetting']['server_port'],
+						array( $vpnserver['VpnServer']['server_port'],
 						       strtolower($network_setting['NetworkSetting']['callsign']),
-							   $network_setting['NetworkSetting']['server_password'],
-							   $network_setting['NetworkSetting']['client_ip'],
-							   $network_setting['NetworkSetting']['server_ip'],
-							   
-							   strtolower($network_setting['NetworkSetting']['client_one_callsign']),
-							   $network_setting['NetworkSetting']['client_one_password'],
-							   $network_setting['NetworkSetting']['client_one_server_ip'],
-							   $network_setting['NetworkSetting']['client_one_ip'],
-							   
-							   strtolower($network_setting['NetworkSetting']['client_two_callsign']),
-							   $network_setting['NetworkSetting']['client_two_password'],
-							   $network_setting['NetworkSetting']['client_two_server_ip'],
-							   $network_setting['NetworkSetting']['client_two_ip'],
-							   
-							   strtolower($network_setting['NetworkSetting']['client_three_callsign']),
-							   $network_setting['NetworkSetting']['client_three_password'],
-							   $network_setting['NetworkSetting']['client_three_server_ip'],
-							   $network_setting['NetworkSetting']['client_three_ip'],
-							   
-							   strtolower($network_setting['NetworkSetting']['client_four_callsign']),
-							   $network_setting['NetworkSetting']['client_four_password'],
-							   $network_setting['NetworkSetting']['client_four_server_ip'],
-							   $network_setting['NetworkSetting']['client_four_ip'],
-							   
-							   strtolower($network_setting['NetworkSetting']['client_five_callsign']),
-							   $network_setting['NetworkSetting']['client_five_password'],
-							   $network_setting['NetworkSetting']['client_five_server_ip'],
-							   $network_setting['NetworkSetting']['client_five_ip'],
-							   
-							   strtolower($network_setting['NetworkSetting']['client_six_callsign']),
-							   $network_setting['NetworkSetting']['client_six_password'],
-							   $network_setting['NetworkSetting']['client_six_server_ip'],
-							   $network_setting['NetworkSetting']['client_six_ip'],
-							   
-							   strtolower($network_setting['NetworkSetting']['client_seven_callsign']),
-							   $network_setting['NetworkSetting']['client_seven_password'],
-							   $network_setting['NetworkSetting']['client_seven_server_ip'],
-							   $network_setting['NetworkSetting']['client_seven_ip'],
-							   
-							   strtolower($network_setting['NetworkSetting']['client_eight_callsign']),
-							   $network_setting['NetworkSetting']['client_eight_password'],
-							   $network_setting['NetworkSetting']['client_eight_server_ip'],
-							   $network_setting['NetworkSetting']['client_eight_ip'],
-							   
-							   strtolower($network_setting['NetworkSetting']['client_nine_callsign']),
-							   $network_setting['NetworkSetting']['client_nine_password'],
-							   $network_setting['NetworkSetting']['client_nine_server_ip'],
-							   $network_setting['NetworkSetting']['client_nine_ip'],
-							   
-							   strtolower($network_setting['NetworkSetting']['client_ten_callsign']),
-							   $network_setting['NetworkSetting']['client_ten_password'],
-							   $network_setting['NetworkSetting']['client_ten_server_ip'],
-							   $network_setting['NetworkSetting']['client_ten_ip']), $vpnclient_conf);
+							   $vpnclient['VpnClient']['password'],
+							   $vpnclient['VpnClient']['client_ip'],
+							   $vonclient['VpnClient']['server_ip']);
 	
 							
-    file_put_contents('/etc/vtund.conf', $vpnclient_conf_output);
+    file_put_contents('/files/VTUND/staging/vtund.conf', $vpnclient_conf_output);
 	}
 	
 	private function render_vtun_config($network_setting) {
-    $vtun_conf = file_get_contents(WWW_ROOT . "/files/VPN/vtun.template");
+    $vtun_conf = file_get_contents(WWW_ROOT . "/files/VTUND/vtun.template");
     $vtun_conf_output = str_replace(array('{server_port}','{callsign}','{server_dns}'),
 	                                     array($network_setting['NetworkSetting']['server_port'],
 										 strtolower($network_setting['NetworkSetting']['callsign']),
 										 $network_setting['NetworkSetting']['server_dns']), $vtun_conf);
 										 
-	file_put_contents('/etc/default/vtun', $vtun_conf_output);
+	file_put_contents('/files/VTUND/staging/vtun', $vtun_conf_output);
 	}	
 }
 ?>	
